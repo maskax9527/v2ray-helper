05aecf871338b4e0f5dfd3a212e56efec00c0376
maskax9527 <maskax@163.com>
2020-11-20 09:47:35 +0800

    Update v2helper.py

--- v2helper.py
+++ v2helper.py
@@ -25,15 +25,7 @@ def get_trafficeinfo_by_lxml():
     url='http://movie.douban.com/top250?format=text'
     html = requests.get(url).content
     soup = BeautifulSoup(html,'html.parser',from_encoding='utf-8')
-    print("豆瓣电影TOP250" + "\n" +" 影片名              评分       评价人数     链接 ")    
-    for tag in soup.find_all('div', class_='info'):    
-        m_name = tag.find('span', class_='title').get_text()        
-        m_rating_score = float(tag.find('span',class_='rating_num').get_text())          
-        m_people = tag.find('div',class_="star")  
-        m_span = m_people.findAll('span')  
-        m_peoplecount = m_span[3].contents[0]  
-        m_url=tag.find('a').get('href')  
-        print( m_name+"        "  +  str(m_rating_score)   + "           " + m_peoplecount + "    " + m_url )       "  +  str(m_rating_score)   + "           " + m_peoplecount + "    " + m_url )   
+    pring(soup)
     
 def main():
     s = requests.session()
